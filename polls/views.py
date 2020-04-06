97c5cc4eca7b41c9c08a9d33f4eecb8ca526a509
Jay VanPut <jvp119@optonline.net>
2020-04-06 19:12:07 -0400

    Add templates and urls to polls app

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import Http404
 
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world. You're in the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list':latest_question_list,
+               'test_var':1}
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}s."
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}s.".format(question_id))
